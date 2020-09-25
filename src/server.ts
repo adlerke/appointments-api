b643be48bf38db613dd28c7b338009d09f8ac410
Adler Kesley <adlerbassk@gmail.com>
2020-09-25 20:52:06 -0300

    FIX : cors fixed

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata'
 
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 import uploadConfig from './config/upload'
 
 import './database'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.diectory))
 app.use(routes)
